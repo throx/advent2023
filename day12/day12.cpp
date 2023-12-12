028c8bee06c746c9ea6a8bf83a2be87578de779f
John Wiltshire <jw@chase.net.au>
2023-12-12 21:47:46 +1000

    Day 12 done (added memoization)

--- day12/day12.cpp
+++ day12/day12.cpp
@@ -3,19 +3,46 @@
 #include <regex>
 #include <conio.h>
 #include <numeric>
+#include <map>
 #include "../shared/Split.h"
 
 using namespace std;
 
 const regex INPUT_R("(\\S+) (\\S+)");
 
-int xx;
+// Need to remember the state of the search so we can memoize the function (ie cache the answer
+// for a particular state of computation) because it gets to the same spot a LOT
+struct State {
+    int pos;        // Position in the input
+    int group;      // Group number we're currently checking
+    int count;      // Current count of springs
+    char next;      // What the next character is (because it's different from the constant input data)
 
-int perms(char* springs, int len, const vector<int>& groups) {
-    int group = 0;
-    int count = 0;
-    int left = accumulate(groups.begin(), groups.end(), 0);
-    for (int x = 0; x < len; ++x) {
+    // C++ can't make its own less than operators (sigh)
+    bool operator<(const State& other) const {
+        if (pos > other.pos) return false;
+        if (pos < other.pos) return true;
+        if (group > other.group) return false;
+        if (group < other.group) return true;
+        if (count > other.count) return false;
+        if (count < other.count) return true;
+        if (next > other.next) return false;
+        if (next < other.next) return true;
+        return false;
+    }
+};
+
+// Yeah, I should pass this as a parameter.
+map<State, __int64> permcache;
+
+__int64 perms_cached(string springs, const vector<int>& groups, State state);
+
+// Uncached naive implementation - run through and recursively test each '?' with a spring or
+// not a spring.  Maintain state through the recursion to make it easy to cache
+__int64 perms_uncached(string springs, const vector<int>& groups, State state) {
+    int group = state.group;
+    int count = state.count;
+    for (int x = state.pos; x < springs.length(); ++x) {
         char c = springs[x];
         switch (c) {
         case '.':
@@ -23,13 +50,8 @@ int perms(char* springs, int len, const vector<int>& groups) {
                 if (groups[group] != count) {
                     return 0;
                 }
-                left -= count;
                 ++group;
                 count = 0;
-
-                if (left + groups.size() - group > len - x) {
-                    return 0;
-                }
             }
             break;
 
@@ -45,14 +67,9 @@ int perms(char* springs, int len, const vector<int>& groups) {
 
         default:
             springs[x] = '.';
-            int c = perms(springs, len, groups);
+            __int64 c = perms_cached(springs, groups, State({ x, group, count, '.'}));
             springs[x] = '#';
-            c += perms(springs, len, groups);
-            springs[x] = '?';
-            while (x < xx) {
-                cout << '.';
-                --xx;
-            }
+            c += perms_cached(springs, groups, State({ x, group, count, '#' }));
             return c;
         }
     }
@@ -71,32 +88,40 @@ int perms(char* springs, int len, const vector<int>& groups) {
     return 1;
 }
 
-int perms(const string& str, const vector<int>& groups) {
-    char* s = new char[str.length()];
-    memcpy(s, str.data(), str.length());
-    xx = str.length();
-    return perms(s, xx, groups);
-    delete s;
+// Caching function
+__int64 perms_cached(string springs, const vector<int>& groups, State state) {
+    auto it = permcache.find(state);
+    if (it != permcache.end()) {
+        return it->second;
+    }
+
+    __int64 p = perms_uncached(springs, groups, state);
+    permcache[state] = p;
+    return p;
+}
+
+// Wraps the caching function to reset the global cache and pass a constant state on entry
+__int64 perms(string springs, const vector<int>& groups) {
+    permcache.clear();
+    return perms_uncached(springs, groups, State({0, 0, 0, 'x'}));
 }
 
 int main()
 {
-    int part1 = 0;
-    int part2 = 0;
+    __int64 part1 = 0;
+    __int64 part2 = 0;
     string s;
     while (getline(cin, s)) {
         smatch sm;
         if (regex_match(s.cbegin(), s.cend(), sm, INPUT_R)) {
             vector<int> groups = SplitInts(sm[2]);
-            int p = perms(sm[1], groups);
+            __int64 p = perms(sm[1], groups);
             part1 += p;
 
             string s2 = sm[1].str() + "?" + sm[1].str() + "?" + sm[1].str() + "?" + sm[1].str() + "?" + sm[1].str();
             string p2 = sm[2].str() + "," + sm[2].str() + "," + sm[2].str() + "," + sm[2].str() + "," + sm[2].str();
-            cout << endl << s2 << " = " << endl;
             groups = SplitInts(p2);
             p = perms(s2, groups);
-            cout << p << endl;
             part2 += p;
         }
     }
